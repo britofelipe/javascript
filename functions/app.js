bbe09af908926892a7ea0e3ae8398846c439f762
britofelipe <arrudabritofelipe@gmail.com>
2022-11-27 16:02:39 -0300

    arrow functions

--- functions/app.js
+++ functions/app.js
@@ -8,3 +8,17 @@ const sum = function(num1, num2) {return num1 + num2}
 
 // console.log(sum(1,1));
 
+// ARROW FUNCTION
+const greetingArrowFunction = name => `My name is ${name}`;
+const sumArrowFunction = (num1, num2) => num1 + num2;
+
+// multiple lines
+const sumSmallNumers = (num1, num2) => {
+    if (num1 > 10 || num2 > 10) {
+        return -1;
+    } else {
+        return num1 + num2;
+    }
+}
+
+console.log(sumSmallNumers(1,2))
